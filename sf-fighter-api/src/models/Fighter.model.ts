25d7d192797b97c3d03bc24d3ba05888feeb4761
Andrey Tetsuo <tetsuo_miyasato@hotmail.com>
2023-10-02 18:45:31 -0300

    feat(fighters): adjust types and interfaces for Fighter model, initial seed data

--- sf-fighter-api/src/models/Fighter.model.ts
+++ sf-fighter-api/src/models/Fighter.model.ts
@@ -1,10 +1,42 @@
 import { Schema, model } from 'mongoose';
+import { IFighter, special, superArt, uniqueAttacks } from '../interfaces/IFighter';
+
+const specialListSchema = new Schema<special>(
+  {
+    name: { type: String, required: true },
+    numPadNotation: { type: String, required: true},
+    type: { type: String, required: true },
+    od: { type: Boolean, required: false },
+    airOk: { type: Boolean, required: false},
+    followUps: { type: [String], default: undefined },
+  }
+)
+
+const uniqueAttacksSchema = new Schema<uniqueAttacks>(
+  {
+    name: { type: String, required: true },
+    command: { type: String, required: true },
+    followUps: { type: [String], default: undefined }
+  }
+)
+
+const superArtSchema = new Schema<superArt>(
+  {
+    name: { type: String, required: true },
+    numPadNotation: { type: String, required: true},
+    type: { type: String, required: true },
+    cost: { type: Number, required: true },
+  }
+)
 
 const fighterSchema = new Schema<IFighter>(
   {
     name: { type: String, required: true },
-    style: { type: String, required: true},
-    age: { type: Number, required: true},
+    height: { type: String, required: true},
+    weight: { type: String, required: true},
+    specialCommandList: { type: [specialListSchema], required: true },
+    superArts: { type: [superArtSchema], required: true,},
+    uniqueAttacks : { type: [uniqueAttacksSchema], required: true },
   }
 );
 
--- sf-fighter-api/src/seeder/seed.ts
+++ sf-fighter-api/src/seeder/seed.ts
@@ -6,12 +6,59 @@ export async function seed() {
     await Fighter.deleteMany({});
 
     await Fighter.insertMany([
-      { name: 'Ryu', style: 'Karate', age: 41 },
-      { name: 'Blanka', style: 'Jungle Ball', age: 38 },
-      { name: 'Zangief', style: 'Wretle Cossack', age: 41 },
-      { name: 'Ken', style: 'Karate', age: 40 },
-      { name: 'Chun-li', style: 'Kung fu', age: 38 }
+      { 
+        name: 'Ryu', height: "1,75m", weight: "85 kgs",
+        specialCommandList: [
+          { name: "Hadoken", numPadNotation: "236@P", type: "motion", od: true },
+          { name: "Shoryuken", numPadNotation: "623@P", type: "motion", od: true},
+          { name: "Hashogeki", numPadNotation: "214@P", type: "motion", od: true },
+          { name: "Tatsumaki Senpu-Kyaku", numPadNotation: "214@K", type: "motion", od: true, airOk: true },
+          { name: "High Blade Kick", numPadNotation: "236@K", type: "motion", od: true },
+          { name: "Denjin Charge", numPadNotation: "22@P", type: "motion" },
+        ],
+        superArts: [
+          { name: "Shinku Hadoken", numPadNotation: "236236@P", type: "motion", cost: 1 },
+          { name: "Shin Hashogeki", numPadNotation: "214214@P", type: "motion", cost: 2 },
+          { name: "Shin Shoryuken", numPadNotation: "236236@K", type: "motion", cost: 3 }, 
+        ],
+        uniqueAttacks: [
+          { name: "Collarbone Breaker", command: "6MP" },
+          { name: "Solar Plexus Strinke", command: "6HP" },
+          { name: "Short Uppercut", command: "4HP" },
+          { name: "Axe Kick", command: "4HK" },
+          { name: "Whirlwind Kick", command: "6HK" },
+          { name: "High Double Strike", command: "HP_HK" },
+          { name: "Fuwa Triple Strike", command: "MP_LK_HK" },
+        ]
+      },
+      { 
+        name: 'Ken', height: "1,75m", weight: "83 kgs",
+        specialCommandList: [
+          { name: "Hadoken", numPadNotation: "236@P", type: "motion", od: true },
+          { name: "Shoryuken", numPadNotation: "623@P", type: "motion", od: true},
+          { name: "Tatsumaki Senpu-Kyaku", numPadNotation: "214@K", type: "motion", od: true, airOk: true },
+          { name: "Jinrai Kick", numPadNotation: "236@K", type: "motion", od: true, followUps: ["6LK", "6MK", "6HK", "OD6@K"] },
+          { name: "Dragonlash Kick", numPadNotation: "623@K", type: "motion", od: true },
+        ],
+        superArts: [
+          { name: "Dragonlash Flame", numPadNotation: "214214@K", type: "motion", cost: 1 },
+          { name: "Shippu Jinrai-kyaku", numPadNotation: "236236@K", type: "motion", cost: 2 },
+          { name: "Shinryu Reppa", numPadNotation: "236236@P", type: "motion", cost: 3 }, 
+        ],
+        uniqueAttacks: [
+          { 
+            name: "Quickdash", command: "KK", followUps: [
+              "623@P",
+              "623@K",
+              "214@K",
+              "LK", "MK", "HK"
+              ]
+          },
+          { name: "Chin Buster", command: "MP_HP" },
+          { name: "Triple Flash Kicks", command: "MK_MK_HK" },
+        ]
+      },
     ]);
-    console.log("fighters seeded!");
+    console.log("Seed Planted");
   }
 }
